f4fc2c8d1c6e76cd103f13ae6080e82f3eda3107
Szymon975 <61831227+Szymon975@users.noreply.github.com>
2020-06-06 03:23:35 +0200

    Update projekt_2.py

--- projekt_2.py
+++ projekt_2.py
@@ -1,88 +1,147 @@
-import numpy as np
+
 import scipy
-import math as m
-import numpy as np
-from scipy import integrate
-import random
-import multiprocessing
-
-number_of_asteroids = 3
-
-Y = np.zeros(shape=(number_of_asteroids,4))
-
-asteroid_sun_distance=5
-mars_orbiting_speed=24
-
-#24 km/s
-print(Y)
-
-def vec_function(x,y):
-	return np.sqrt(np.power(x,2)-np.power(y,2))
-
-def cylindrical_to_cartesian_transformation(r,theta,Z):
-	Z[:,0]=r*np.cos(theta)
-	Z[:,2]=r*np.sin(theta)
-	return Z
-
-starting_thetas=[random.uniform(0,2*m.pi) for __ in range(number_of_asteroids)]
-'''print(cylindrical_to_cartesian_transformation(asteroid_sun_distance,starting_thetas))'''
-
-#liczymy macierz obrotu od wektora mars_orbiting_speed (wikipedia)
-starting_velocities_r = [-mars_orbiting_speed*m.sin(random.uniform(0,m.pi/2)) for __ in range(number_of_asteroids)]
-starting_velocities_theta = vec_function(mars_orbiting_speed ,starting_velocities_r)
-'''print(starting_velocities_theta)'''
-
-def make_starting_array(r,theta,funct):
-	Y[:,1] = starting_velocities_r
-	Y[:,3] = starting_velocities_theta
-	return funct(r,theta,Y)
-print(make_starting_array(asteroid_sun_distance,starting_thetas,cylindrical_to_cartesian_transformation))
-print(Y[0,1])
-
-sun_sgp=4
-earth_sgp=3
-moon_sqp=2
-sun_coordinates=[0,0]
-earth_sun_distance=3
-step=0.01
-liminal_time=25.0
-radial_earth_speed=1
-def earth_coordinates(theta,funct):
-	return funct(earth_sun_distance,theta,B)
-
-B = np.zeros(shape=(int(liminal_time/step),3))
-B[:,0]= np.arange(0,liminal_time,step)
-#wspolrzedne x-owe
-'''B[:,1]= earth_sun_distance*np.cos(radial_speed*B[:,0])
-B[:,2]= earth_sun_distance*np.sin(radial_speed*B[:,0])'''
-print(B)
+import scipy
+import math
+import math
 
 
+number_of_asteroids = 1000
 
-'''class asteroid_data:
-	def '''
-
-file = open("dane.txt","w+")
-'''data.csv'''
-file.truncate(0)
-file.close()
-
-
-def solvr(Y,t):
-    return[-Y[1],sun_sgp/(np.hypot(Y[0], Y[2])**3)*Y[0],-Y[3],sun_sgp/(np.hypot(Y[0], Y[2])**3)*Y[2]]
-
-def main(i):
-    a_t = np.arange(0, liminal_time, step)
+class vector: # wektor euklidesowy dla oddziaływań Słońce-Ziemia-Księżyc
+class vector: # wektor euklidesowy dla oddziaływań Słońce-Ziemia-Księżyc
+	x = float()
+	x = float()
+	y = float()
+	y = float()
 
 
-    asol = integrate.odeint(solvr, [Y[i,0],Y[i,1],Y[i,2],Y[i,3]], a_t)
-    astack = np.c_[a_t, asol[:,0], asol[:, 1],asol[:,2], asol[:, 3]]
-    np.savetxt('dane.txt', astack, delimiter=',', header='t, dx,vx,dy,vy', comments='')
+	def __init__(x_arg: float, y_arg: float):
+	def __init__(x_arg: float, y_arg: float):
+		x, y = x_arg, y_arg
+		x, y = x_arg, y_arg
 
-processes = []
-i=0
-for _ in range(number_of_asteroids):
-	p= multiprocessing.Process(target=main(i))
-	p.start()
-	i+=1
-	processes.append(p)
+
+	def __init__():
+		x, y = 0, 0
+
+	def lenght():
+	def lenght():
+		return math.hypot(x,y)
+		return math.hypot(x,y)
+
+
+	def __add__(self, vec2: vector):
+	def __add__(self, vec2):
+		return vector(self.x + vec2.x, self.y + vec2.y)
+		return vector(self.x + vec2.x, self.y + vec2.y)
+
+
+	def __sub__(self, vec2: vector):
+	def __sub__(self, vec2):
+		return vector(self.x - vec2.x, self.y - vec2.y)
+		return vector(self.x - vec2.x, self.y - vec2.y)
+
+
+	def __iadd__(self, vec2: vector):
+	def __iadd__(self, vec2):
+		temp = self + vec2
+		temp = self + vec2
+		x = temp.x
+		x = temp.x
+		y = temp.y
+		y = temp.y
+		return temp
+		return temp
+
+
+	def __isub__(self, vec2: vector):
+	def __isub__(self, vec2):
+		temp = self - vec2
+		temp = self - vec2
+		x = temp.x
+		x = temp.x
+		y = temp.y
+		y = temp.y
+@@ -38,18 +43,64 @@ def __div__(self, divisor: float):
+
+
+
+
+class celestial_body: # ciało Słońce/Ziemia/Księżyc
+class celestial_body: # ciało Słońce/Ziemia/Księżyc
+	position = vector()
+	position : vector
+	velocity = vector()
+	velocity : vector
+	acceleration = vector()
+	acceleration : vector
+	sgp = 0. # standardowy parametr grawitacyjny
+	sgp = 0. # standardowy parametr grawitacyjny
+	radius = 0.
+	radius = 0.
+
+
+	def interact(self, cb2: celestial_body):
+	def interact(self, cb2):
+		vec_to_cb2 = cb2.position - self.position
+		vec_to_cb2 = cb2.position - self.position
+		distance = (self.position - cb2.position).lenght()
+		distance = (self.position - cb2.position).lenght()
+		acceleration += vec_to_cb2 * sgp / distance ** 3
+		acceleration += vec_to_cb2 * sgp / distance ** 3
+
+
+	def update():
+	def update():
+		velocity += acceleration
+		velocity += acceleration
+		position += velocity
+		position += velocity
+		acceleration = vector(0.,0.)
+		acceleration = vector(0.,0.)
+
+
+
+class simualtion:
+	asteroid_x = np.zeros(shape=(number_of_asteroids)) # numpy array'e asteroidów wrzuci się do oddzielnej klasy
+	asteroid_y = np.zeros(shape=(number_of_asteroids))
+	asteroid_velocity_x = np.zeros(shape=(number_of_asteroids))
+	asteroid_velocity_y = np.zeros(shape=(number_of_asteroids))
+	asteroid_acceleration_x = np.zeros(shape=(number_of_asteroids))
+	asteroid_acceleration_y = np.zeros(shape=(number_of_asteroids))
+	Earth : celestial_body
+	Sun : celestial_body
+	Moon : celestial_body
+	collision_angles = []
+	asteroid_collision_mask = np.ones(shape=(number_of_asteroids)) # maska do oznaczania, czy asteroida uderzyła w ciało
+
+	def __init__():
+		# tu będzie implementacja rozmieszczania ciał niebieskich i asteroid
+		return
+
+	def simulate(): # główna metoda symulacji
+		while True: #warunek przzerwania symulacji zostanie dodany potem
+			#interakcje grawitacyjne ciał
+			Earth.interact(Sun)
+			Earth.interact(Moon)
+			Sun.interact(Earth) # można by przykleić Słońce w punkcie (0,0), do rozważenia
+			Sun.interact(Moon)
+			Moon.interact(Earth)
+			Moon.interact(Sun)
+			for body in [Sun, Earth, Moon]:
+				asteroid_vec_to_body_x = numpy.full((number_of_asteroids), body.position.x) - asteroid_x
+				asteroid_vec_to_body_y = numpy.full((number_of_asteroids), body.position.y) - asteroid_y
+				asteroid_distance = np.hypot(asteroid_vec_to_body_x, asteroid_vec_to_body_y)
+				asteroid_acceleration_x += asteroid_vec_to_body_x * body.sgp / asteroid_distance ** 3
+				asteroid_acceleration_y += asteroid_vec_to_body_y * body.sgp / asteroid_distance ** 3
+			#aktualizacje pozycji
+			Earth.update()
+			Moon.update()
+			Sun.update()
+			asteroid_velocity_x += asteroid_acceleration_x
+			asteroid_velocity_y += asteroid_acceleration_y
+			asteroid_x += asteroid_velocity_x
+			asteroid_y += asteroid_velocity_y
+			asteroid_acceleration_x = np.zeros(shape=(number_of_asteroids)) #czyszczenie przypieszeń po kroku
+			asteroid_acceleration_y = np.zeros(shape=(number_of_asteroids))
+			#kolizje ogarnie się potem 
